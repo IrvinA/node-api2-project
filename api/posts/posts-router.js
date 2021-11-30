6ef2758bffab0a62fce72142d3ab77cb811b00ca
IrvinA <arevalosirvin@gmail.com>
2021-11-30 09:40:39 -0800

    post endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -34,4 +34,39 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      message: 'Please provide tittle and contents for the post',
+    });
+  } else {
+    Post.insert({ title, contents })
+      .then((post) => {
+        Post.findById(post.id)
+          .then((post) => {
+            if (!post) {
+              res.status(404).json({
+                message: 'The post with the specified ID does not exist',
+              });
+            } else {
+              res.status(201).json(post);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            res.status(500).json({
+              message: 'The post information could not be retrieved',
+            });
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          message: 'There was an error while saving the post to the database',
+        });
+      });
+  }
+});
+
 module.exports = router;
