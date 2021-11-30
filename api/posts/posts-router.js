d4aa7a5604c78e5e83991899cb85df57f5442566
IrvinA <arevalosirvin@gmail.com>
2021-11-30 09:32:54 -0800

    get by id endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -15,4 +15,23 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (!post) {
+        res.status(400).json({
+          message: 'The post with the specified ID does not exist',
+        });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: 'The post information could not be retrieved',
+      });
+    });
+});
+
 module.exports = router;
