b943088771ee2db96609408a98d720d24a092c49
IrvinA <arevalosirvin@gmail.com>
2021-11-30 09:51:51 -0800

    delete endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -94,4 +94,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    if (!post) {
+      res.status(404).json({
+        message: 'The post with the specified ID does not exist',
+      });
+    } else {
+      await Post.remove(id);
+      res.status(200).json(post);
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'The post could not be removed',
+    });
+  }
+});
+
 module.exports = router;
